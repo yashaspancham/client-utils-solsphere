16abce32367edb747581a957fd8a212e84102fbb
Yashas <yashaspancham@gmail.com>
2025-09-09 10:45:21 +0530

    Yashas-feat-WindowsForforgroundOnly

--- agent.py
+++ agent.py
@@ -46,7 +46,6 @@ def run_daemon():
         print(json.dumps(report, indent=6))
         checks_str = json.dumps(report["checks"], sort_keys=True)
         # send_report(report) #forTesting
-
         if checks_str != last_state:
             print("Change detected, sending report...")
             send_report(report)
@@ -57,7 +56,7 @@ def run_daemon():
         time.sleep(CHECK_INTERVAL)
 
 
-def daemonize():
+def daemonize_linux_macOS():
     if os.fork():
         sys.exit()
 
@@ -74,6 +73,28 @@ def daemonize():
         os.dup2(devnull.fileno(), sys.stderr.fileno())
 
 
+def run_windows_daemon():
+    global last_state
+    while True:
+        report = collect_system_checks("windows")
+        checks_str = json.dumps(report["checks"], sort_keys=True)
+
+        if checks_str != last_state:
+            print("Change detected, sending report...")
+            send_report(report)
+            last_state = checks_str
+        else:
+            print("No changes, skipping send.")
+
+        time.sleep(CHECK_INTERVAL)
+
+
 if __name__ == "__main__":
-    daemonize()
-    run_daemon()
+    os_type = platform.system().lower()
+    if os_type == "linux" or os_type == "darwin":
+        daemonize_linux_macOS()
+        run_daemon()
+    elif os_type == "windows":
+        run_windows_daemon()
+    else:
+        print(f"Unsupported OS: {os_type}")
