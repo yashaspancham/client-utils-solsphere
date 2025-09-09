764b71c584204d6199789ad6dd77f229ea8552d2
Yashas <yashaspancham@gmail.com>
2025-09-09 09:10:10 +0530

    Yashas-feat-sendReportAPIIntegrated

--- agent.py
+++ agent.py
@@ -1,32 +1,38 @@
 import json
+import requests
 import platform
 import socket
-from datetime import datetime,timezone
+from datetime import datetime, timezone
 from checksfuns import (
     get_disk_encryption_status,
     get_os_updates_status,
     get_antivirus_status,
-    get_inactivity_timeout
+    get_inactivity_timeout,
 )
+from sendReport import send_report
+
 
 def collect_basic_state():
-    os=platform.system().lower()
-    dist_encryption_status=get_disk_encryption_status(os_type=os)
-    os_updates_info=get_os_updates_status(os_type=os)
-    anti_virus_info=get_antivirus_status(os_type=os)
-    inactivity_timeout_info=get_inactivity_timeout(os_type=os)
+    os = platform.system().lower()
+    dist_encryption_status = get_disk_encryption_status(os_type=os)
+    os_updates_info = get_os_updates_status(os_type=os)
+    anti_virus_info = get_antivirus_status(os_type=os)
+    inactivity_timeout_info = get_inactivity_timeout(os_type=os)
     state = {
-        "machineId": "demo-machine-id",
-        "os": os,   # 'linux', 'darwin', 'windows'
-        "hostname": socket.gethostname(),
-        "lastCheckedAt": datetime.now(timezone.utc).isoformat(),
-        "diskEncryption": dist_encryption_status,
-        "osUpdates": os_updates_info,
-        "anitivirus": anti_virus_info,
-        "inactivityTimeout": inactivity_timeout_info
+        "os_type": os,  # 'linux', 'darwin', 'windows'
+        "machine_id": socket.gethostname(),
+        "checks": {
+            "lastCheckedAt": datetime.now(timezone.utc).isoformat(),
+            "diskEncryption": dist_encryption_status,
+            "osUpdates": os_updates_info,
+            "anitivirus": anti_virus_info,
+            "inactivityTimeout": inactivity_timeout_info,
+        },
     }
     return state
+
 
 if __name__ == "__main__":
     state = collect_basic_state()
     print(json.dumps(state, indent=6))
+    send_report(state)
---
+++ sendReport.py
@@ -0,0 +1,28 @@
+import requests
+import socket
+import platform
+import json
+import os
+from env import CONFIG_ENV
+
+
+API_URL = CONFIG_ENV['SEND_REPORT_API']
+
+
+def send_report(checks: dict):
+    print("API_URL:", API_URL)
+    payload = {
+        "report": checks
+    }
+
+    try:
+        response = requests.post(API_URL, json=payload, timeout=10)
+        if response.status_code == 200:
+            print("Report sent successfully")
+            return True
+        else:
+            print(f"Failed to send report: {response.status_code} {response.text}")
+            return False
+    except Exception as e:
+        print(f"⚠️ Error sending report: {e}")
+        return False
