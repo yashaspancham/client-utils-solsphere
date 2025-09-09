61da2660ee3f580877d8a9e945f31443b9539cd3
Yashas <yashaspancham@gmail.com>
2025-09-09 10:10:46 +0530

    Yashas-feat-linuxAndMacOsDone

--- agent.py
+++ agent.py
@@ -1,8 +1,11 @@
+import os
+import sys
 import json
-import requests
 import platform
-import socket
+import requests
 from datetime import datetime, timezone
+import socket
+import time
 from checksfuns import (
     get_disk_encryption_status,
     get_os_updates_status,
@@ -12,7 +15,7 @@ from checksfuns import (
 from sendReport import send_report
 
 
-def collect_basic_state():
+def collect_system_checks():
     os = platform.system().lower()
     dist_encryption_status = get_disk_encryption_status(os_type=os)
     os_updates_info = get_os_updates_status(os_type=os)
@@ -21,8 +24,8 @@ def collect_basic_state():
     state = {
         "os_type": os,  # 'linux', 'darwin', 'windows'
         "machine_id": socket.gethostname(),
+        "lastCheckedAt": datetime.now(timezone.utc).isoformat(),
         "checks": {
-            "lastCheckedAt": datetime.now(timezone.utc).isoformat(),
             "diskEncryption": dist_encryption_status,
             "osUpdates": os_updates_info,
             "anitivirus": anti_virus_info,
@@ -32,7 +35,45 @@ def collect_basic_state():
     return state
 
 
+CHECK_INTERVAL = 60 * (1 / 8)  # 60*x = x minutes(in seconds)
+last_state = None
+
+
+def run_daemon():
+    global last_state
+    while True:
+        report = collect_system_checks()
+        print(json.dumps(report, indent=6))
+        checks_str = json.dumps(report["checks"], sort_keys=True)
+        # send_report(report) #forTesting
+
+        if checks_str != last_state:
+            print("Change detected, sending report...")
+            send_report(report)
+            last_state = checks_str
+        else:
+            print("No changes, skipping send.")
+
+        time.sleep(CHECK_INTERVAL)
+
+
+def daemonize():
+    if os.fork():
+        sys.exit()
+
+    os.setsid()
+    if os.fork():
+        sys.exit()
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+    with open("/dev/null", "r") as devnull:
+        os.dup2(devnull.fileno(), sys.stdin.fileno())
+    with open("/dev/null", "a+") as devnull:
+        os.dup2(devnull.fileno(), sys.stdout.fileno())
+        os.dup2(devnull.fileno(), sys.stderr.fileno())
+
+
 if __name__ == "__main__":
-    state = collect_basic_state()
-    print(json.dumps(state, indent=6))
-    send_report(state)
+    daemonize()
+    run_daemon()
